7b185095e09277b515b40a74f893e5e0de5839b6
Rémi <rfaivre91@gmail.com>
2016-02-22 01:53:58 +0100

    add config capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'website'
+set :repo_url, 'pi@192.168.1.24:remote/git/remi'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/sites/remi'
 
 # Default value for :scm is :git
 # set :scm, :git
